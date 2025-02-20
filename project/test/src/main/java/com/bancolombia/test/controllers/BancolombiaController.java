080b017862513a261316e512c8d08b1a8a46030a
jhreyes483 <jhreyes483@misena.edu.co>
2025-02-20 18:46:51 -0500

    pasar varibles a la vista

--- project/test/src/main/java/com/bancolombia/test/controllers/BancolombiaController.java
+++ project/test/src/main/java/com/bancolombia/test/controllers/BancolombiaController.java
@@ -1,6 +1,7 @@
 package com.bancolombia.test.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BancolombiaController {
     @GetMapping("/detalles_info")
-    public String info(){
+    public String info(Model model){
+        // Aquí se agregan variables al modelo
+        model.addAttribute("nombre", "Juan Pérez");  // Agregar un atributo 'nombre' con valor
+        model.addAttribute("edad", 30);              // Agregar un atributo 'edad' con valor
+
+        // Puedes agregar tantos atributos como necesites
+        model.addAttribute("empresa", "Bancolombia");
         return "detalles_info";
     }
     
