7243f08b75d3a57efe7b52676f1e926784fae33e
jhreyes483 <jhreyes483@misena.edu.co>
2025-02-20 19:12:37 -0500

    endpoint usando RequestMaping

--- project/test/src/main/java/com/bancolombia/test/controllers/BancolombiaRestController.java
+++ project/test/src/main/java/com/bancolombia/test/controllers/BancolombiaRestController.java
@@ -6,13 +6,22 @@ import java.util.Map;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @RestController
+@RequestMapping("/api")
+
+
 public class BancolombiaRestController {
-    @GetMapping("/detalles_info2")
+
+    @RequestMapping(path="/detalles_info2", method=RequestMethod.GET) 
+       
     public Map<String, Object>detalles_info2(){
         Map<String, Object> respuesta = new HashMap<>();
         respuesta.put("nombre", "Juan Pérez"); 
